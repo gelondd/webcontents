2af93604344a347e1c16c7cbb528fb3d78438bd0
gelondd <joeunseo147@naver.com>
2025-04-01 12:26:49 +0900

    Update calculate1.c

--- calculate1.c
+++ calculate1.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
 int main()
 {
-	int won = 500000; /*�����ϴ� ���ÿ� ���� �ʱ�ȭ, ��� �����ؼ� ������ �ڷ��� ��� ��� ������ ; ���*/
-	int dalar = won / 1221.0; /*���Թ��ȿ��� ���� ���� ex 500,00 ��� �Ұ��� */
+	int won = 500000; /*제시하는 동시에 대입 초기화, 결과 생각해서 적절한 자료형 사용 모든 문에는 ; 사용*/
+	int dalar = won / 1221.0; /*대입문안에는 숫자 단위 ex 500,00 사용 불가능 */
 	
-	printf("��ȭ %d���� �޷� ���� �ݾ����� ȯ���ϸ�\n %d�޷��̴�.", won, dalar);
+	printf("원화 %d원을 달러 단위 금액으로 환전하면\n %d달러이다.", won, dalar);
 
 	
 	return 0;
+
+
+#pragma warning (disable:4996)
+
+#include <stdio.h>
+int main()
+{
+	int C;
+	double F;
+	/*순서가 중요*/
+	printf("섭씨온도를 정수값으로 넣어주십시오.\n");
+	scanf("%d",&C);
+	F = 9.0 / 5.0 * C + 32;
+	printf("섭씨온도 %d를 \n 화씨 온도로 출력하면 %f입니다.", C, F);
+
+	return 0;
+
+
 }
