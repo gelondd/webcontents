586cbff2c6d031156d88f98f544f6b01ddb31763
gelondd <joeunseo147@naver.com>
2025-04-01 12:52:19 +0900

    Update calculate1.c

--- calculate1.c
+++ calculate1.c
@@ -27,3 +27,24 @@ int main()
 
 
 }
+#pragma warning (disable:4996)
+
+#include <stdio.h>
+int main()
+{
+	int r, e, m;
+	double average;
+	/*순서가 중요*/
+	printf("순서대로 국어, 영어, 수학 점수를 입력해주세요. 숫자를 치고 숫자 뒤에 바로 컴마를 꼭 넣어주십시오.\n");
+	scanf("%d, %d, %d" , &r, &e, &m);
+	average = (r + e + m) / 3.0;
+	printf(" 국어,영어, 수학의 점수는 각각 %d,%d,%d입니다. \n 세 과목의 평균은 %f입니다. ", r,e,m,average);
+	printf(" 국어,영어, 수학의 점수는 각각 %d,%d,%d입니다. \n 세 과목의 평균은 %f입니다. ", r, e, m, (r + e + m) / 3.0);
+
+	int tot = r + e + m; /*중간변수 존재 ->프로그램의 기능 확장을 염두에 둠!*/
+	printf(" 국어,영어, 수학의 점수는 각각 %d,%d,%d입니다. \n 세 과목의 평균은 %f입니다. ", r, e, m, tot / 3.0);
+
+	return 0;
+
+
+}
